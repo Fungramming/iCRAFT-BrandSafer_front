cc6a36de389ef2c619fac1982bb300492eca082a
hironobu <ghdtjs0417@gmail.com>
2018-07-24 15:04:04 +0900

    Add: constant.js

---
+++ src/constant.js
@@ -0,0 +1,76 @@
+export default {
+    /* ========= User ========= */
+    // Auth
+    LOG_IN: "logIn",
+    LOG_OUT: "logOut",
+    // Password
+    UPDATE_PASSWORD: "updatePassword",
+    RESET_PASSWORD: "resetPassword",
+    //막 쓰기
+    // 기간설정, 고객사 선택
+    FETCH_STATUS: "fetchDashboard",
+    // 기간설정
+    FETCH_PRODUCT_CERIT: "fectchProductCert",
+
+    FETCH_PRODUCT: "fetchProduct",
+    ADD_PRODUCT: "addProduct",
+    UPDATE_PRODUCT: "updateProduct",
+    DELETE_PRODUCT: "deleteProduct",
+
+    FETCH_APP_LELEASE: "fetchAppLelease",
+    ADD_APP_LELEASE: "addAppLelease",
+    UPDATE_APP_LELEASE: "updateAppLelease",
+    DELETE_APP_LELEASE: "deleteAppLelease",
+
+    FETCH_APP: "fectchApp",
+
+    FETCH_CUSTOMER: "fetchCustomer",
+    ADD_CUSTOMER: "addCustomer",
+    UPDATE_CUSTOMER: "updateCustomer",
+
+    FETCH_ICRAFT_USER: "fetchIcraftUser",
+    ADD_ICRAFT_USER: "addIcraftUser",
+    UPDATE_ICRAFT_USER: "updateIcraftUser",
+    DELETE_ICRAFT_USER: "deleteIcraftUser",
+
+    FETCH_USER: "fetchUser",
+    ADD_USER: "addUser",
+    UPDATE_USER: "updateUser",
+    DELETE_USER: "deleteUser",
+
+    FETCH_ACCOUNT_LOG: "fetchAccountLog",
+
+    // 권한관리 서버와 의논 필요
+
+    FETCH_BLACKLIST: "fetchBlacklist",
+    ADD_BLACKLIST: "addBlacklist",
+    DELETE_BLACKLIST: "deleteBlacklist",
+
+    FETCH_OVER_CERT: "fetchOverCert",
+    FETCH_RANDOM_NUMBER: "fetchRandomNumber",
+    ADD_RANDOM_NUMBER: "addRandomNumber",
+    UPDATE_RANDOM_NUMBER: "updateRandomNumber",
+
+    // 난수정보 조회가 뭐죠??
+    // 관리자 app 등록은 어디서 하나요?
+
+    FETCH_ADMIN_APP: "fetchAdminApp",
+    UPDATE_ADMIN_APP: "updateAdminApp",
+    DELETE_ADMIN_APP: "deleteAdminApp",
+
+    FETCH_DISTRIBUTOR: "fetchDistributor",
+    ADD_DISTRIBUTOR: "addDistributor",
+    UPDATE_DISTRIBUTOR: "updateDistributor",
+    DELETE_DISTRIBUTOR: "deleteDistributor",
+
+    FETCH_TAG_TYPE: "fetchTagType",
+    ADD_TAG_TYPE: "addTagType",
+    UPDATE_TAG_TYPE: "updateTagType",
+    DELETE_TAG_TYPE: "deleteTagType",
+
+    // 코스토리 유통사 코드 띠용
+
+    FETCH_BLACKLISI_REGION: "fetchBlacklistRegion",
+    ADD_BLACKLISI_REGION: "addBlacklistRegion",
+    DELETE_BLACKLISI_REGION: "deleteBlacklistRegion"
+};
