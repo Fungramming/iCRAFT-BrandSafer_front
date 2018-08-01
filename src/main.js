74f71ec29757e6a5d1a25a5f4e7b02790e996063
CHOISHO <choisho1136@gmail.com>
2018-08-01 17:43:56 +0900

    adding store folder

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 import Vue from "vue";
 import App from "./App.vue";
+import ES6Promise from "es6-promise";
+ES6Promise.polyfill();
 
 import axios from "axios";
 import router from "./router";
 import store from "./store/index.js";
 
-// MODULE CSS
+// CSS MODULE
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import "vuetify/dist/vuetify.min.js";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
-
-import DatePicker from "vue2-datepicker";
-
 Vue.use(Vuetify);
-Vue.prototype.$axios = axios;
-Vue.config.productionTip = false;
 
+// CALENDAR
+import DatePicker from "vue2-datepicker";
 Vue.use(DatePicker);
+
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL = "http://localhost:5000";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,163 @@
+import BSAPI from "../api/BSAPI";
 import Constant from "../constant";
 
+function promise(apiFunction, payload, store, constant) {
+  store.dispatch(Constant.SHOW_LOADING, true);
+
+  return new Promise((resolve, reject) => {
+    apiFunction(payload)
+      .then(resp => {
+        if (constant) {
+          store.commit(constant, resp.data);
+        }
+        resolve(resp);
+        store.dispatch(Constant.SHOW_LOADING, false);
+      })
+      .catch(err => {
+        reject(err);
+        store.dispatch(Constant.SHOW_LOADING, false);
+      });
+  });
+}
 export default {
   [Constant.LOG_IN]: (store, payload) => {
     store.commit(Constant.LOG_IN, payload);
   },
   [Constant.LOG_OUT]: store => {
     store.commit(Constant.LOG_OUT);
+  },
+  async [Constant.LOG_IN](store, payload) {
+    return promise(BSAPI.logIn, payload, store, Constant.LOG_IN);
+  },
+  // Dashboard
+  async [Constant.FETCH_DASHBOARD](store, payload) {
+    return promise(BSAPI.fetchDashboard, payload, store, null);
+  },
+  async [Constant.FETCH_USER_DASHBOARD](store, payload) {
+    return promise(BSAPI.fetchUserDashboard, payload, store, null);
+  },
+  // Product
+  async [Constant.FETCH_PRODUCT_CERIT](store, payload) {
+    return promise(BSAPI.fectchProductCert, payload, store, null);
+  },
+  // App
+  async [Constant.FETCH_APP_LELEASE](store, payload) {
+    return promise(BSAPI.fetchAppLelease, payload, store, null);
+  },
+  async [Constant.ADD_APP_LELEASE](store, payload) {
+    return promise(BSAPI.addAppLelease, payload, store, null);
+  },
+  async [Constant.UPDATE_APP_LELEASE](store, payload) {
+    return promise(BSAPI.updateAppLelease, payload, store, null);
+  },
+  async [Constant.DELETE_APP_LELEASE](store, payload) {
+    return promise(BSAPI.deleteAppLelease, payload, store, null);
+  },
+  //Admin
+  async [Constant.FETCH_CUSTOMER](store, payload) {
+    return promise(BSAPI.fetchCustomer, payload, store, null);
+  },
+  async [Constant.ADD_CUSTOMER](store, payload) {
+    return promise(BSAPI.addCustomer, payload, store, null);
+  },
+  async [Constant.UPDATE_CUSTOMER](store, payload) {
+    return promise(BSAPI.updateCustomer, payload, store, null);
+  },
+
+  async [Constant.FETCH_ICRAFT_USER](store, payload) {
+    return promise(BSAPI.fetchIcraftUser, payload, store, null);
+  },
+  async [Constant.FETCH_ICRAFT_USER_ID](store, payload) {
+    return promise(BSAPI.fetchIcraftUserId, payload, store, null);
+  },
+  async [Constant.ADD_ICRAFT_USER](store, payload) {
+    return promise(BSAPI.addIcraftUser, payload, store, null);
+  },
+  async [Constant.UPDATE_ICRAFT_USER](store, payload) {
+    return promise(BSAPI.updateIcraftUser, payload, store, null);
+  },
+  async [Constant.DELETE_ICRAFT_USER](store, payload) {
+    return promise(BSAPI.deleteIcraftUser, payload, store, null);
+  },
+
+  async [Constant.FETCH_USER](store, payload) {
+    return promise(BSAPI.fetchUser, payload, store, null);
+  },
+  async [Constant.ADD_USER](store, payload) {
+    return promise(BSAPI.addUser, payload, store, null);
+  },
+  async [Constant.UPDATE_USER](store, payload) {
+    return promise(BSAPI.updateUser, payload, store, null);
+  },
+  async [Constant.DELETE_USER](store, payload) {
+    return promise(BSAPI.deleteUser, payload, store, null);
+  },
+
+  async [Constant.FETCH_ACCOUNT_LOG](store, payload) {
+    return promise(BSAPI.fetchAccountLog, payload, store, null);
+  },
+
+  async [Constant.FETCH_BLACKLIST](store, payload) {
+    return promise(BSAPI.fetchBlacklist, payload, store, null);
+  },
+  async [Constant.ADD_BLACKLIST](store, payload) {
+    return promise(BSAPI.addBlacklist, payload, store, null);
+  },
+  async [Constant.DELETE_BLACKLIST](store, payload) {
+    return promise(BSAPI.deleteBlacklist, payload, store, null);
+  },
+
+  async [Constant.FETCH_OVER_CERT](store, payload) {
+    return promise(BSAPI.fetchOverCert, payload, store, null);
+  },
+
+  async [Constant.FETCH_RANDOM_NUMBER](store, payload) {
+    return promise(BSAPI.fetchRandomNumber, payload, store, null);
+  },
+  async [Constant.ADD_RANDOM_NUMBER](store, payload) {
+    return promise(BSAPI.addRandomNumber, payload, store, null);
+  },
+  async [Constant.UPDATE_RANDOM_NUMBER](store, payload) {
+    return promise(BSAPI.updateRandomNumber, payload, store, null);
+  },
+
+  async [Constant.FETCH_ADMIN_APP](store, payload) {
+    return promise(BSAPI.fetchAdminApp, payload, store, null);
+  },
+  async [Constant.ADD_ADMIN_APP](store, payload) {
+    return promise(BSAPI.addAdminApp, payload, store, null);
+  },
+  async [Constant.UPDATE_ADMIN_APP](store, payload) {
+    return promise(BSAPI.updateAdminApp, payload, store, null);
+  },
+
+  async [Constant.FETCH_DISTRIBUTOR](store, payload) {
+    return promise(BSAPI.fetchDistributor, payload, store, null);
+  },
+  async [Constant.ADD_DISTRIBUTOR](store, payload) {
+    return promise(BSAPI.addDistributor, payload, store, null);
+  },
+  async [Constant.UPDATE_DISTRIBUTOR](store, payload) {
+    return promise(BSAPI.updateDistributor, payload, store, null);
+  },
+  async [Constant.DELETE_DISTRIBUTOR](store, payload) {
+    return promise(BSAPI.deleteDistributor, payload, store, null);
+  },
+
+  async [Constant.FETCH_TAG_TYPE](store, payload) {
+    return promise(BSAPI.fetchTagType, payload, store, null);
+  },
+  async [Constant.ADD_TAG_TYPE](store, payload) {
+    return promise(BSAPI.addTagType, payload, store, null);
+  },
+  async [Constant.UPDATE_TAG_TYPE](store, payload) {
+    return promise(BSAPI.updateTagType, payload, store, null);
+  },
+  async [Constant.DELETE_TAG_TYPE](store, payload) {
+    return promise(BSAPI.deleteTagType, payload, store, null);
+  },
+
+  [Constant.SHOW_LOADING]: (state, isLoading) => {
+    state.isLoading = isLoading;
   }
 };
