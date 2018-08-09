74d624895367c3dd19047959e15c1e59065e2eee
blackrubycode <cccy023@naver.com>
2018-08-09 19:25:41 +0900

    try post function

--- src/components/CompHelper.js
+++ src/components/CompHelper.js
@@ -8,9 +8,7 @@ export function getTotal(_this) {
   _this.total = update_total;
 
   let pageNum = _this.$children[0].$children[4].value - 1;
-  console.log("pageNum :", pageNum);
   let pageActiveText = _this.$children[0].$children[1].$children[1].value;
-  console.log("pageActiveText :", pageActiveText);
   let calPage = pageNum * pageActiveText;
   _this.total_index = calPage;
 }
